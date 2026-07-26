e393c4c924d341cc9f0f00cbc353675d6574324b
Emilis Voropajevas <emilisvoropajevas@gmail.com>
2026-07-26 02:55:54 +0100

    Added queryfields for returning a single task and returning a filtered complete status task

--- src/schema/task.ts
+++ src/schema/task.ts
@@ -16,6 +16,42 @@ builder.prismaObject('Task', {
 })
 
 builder.queryFields((t) => ({
+    // Return single task
+    getSingleTask: t.prismaField({
+        type: 'Task',
+        args: {
+            taskId: t.arg.id({ required: true }),
+        },
+        resolve: async (query, root, args, ctx) => {
+            return prisma.task.findUniqueOrThrow({
+                ...query,
+                where: {
+                    id: parseInt(args.taskId)
+                }
+            })
+        }
+    }),
+    // Return Tasks Filtered by Completion
+    getFilterCompleteTasks: t.prismaField({
+        type: ['Task'],
+        args: {
+            taskListId: t.arg.id({ required: true }),
+            completed: t.arg.boolean({ required: false }),
+        },
+        resolve: async (query, root, args, ctx) => {
+            return prisma.task.findMany({
+                ...query,
+                where: {
+                    tasklistId: parseInt(args.taskListId),
+                    ...(args.completed !== undefined && args.completed !== null
+                        ? { completed: args.completed }
+                        : {}
+                     ),
+                }
+            })
+        }
+
+    })
 
 }))
 
@@ -41,14 +77,14 @@ builder.mutationFields((t) => ({
     updateTaskName: t.prismaField({
         type: 'Task',
         args: {
-            id: t.arg.id({ required: true }),
+            taskId: t.arg.id({ required: true }),
             title: t.arg.string({ required: true }),
         },
         resolve: async (query, root, args, ctx) => {
             return prisma.task.update({
                 ...query,
                 where: {
-                    id: parseInt(args.id),
+                    id: parseInt(args.taskId),
                 },
                 data: {
                     title: args.title,
@@ -60,14 +96,14 @@ builder.mutationFields((t) => ({
     updateTaskStatus: t.prismaField({
         type: 'Task',
         args: {
-            id: t.arg.id({ required: true }),
+            taskId: t.arg.id({ required: true }),
             completed: t.arg.boolean({ required: true }),
         },
         resolve: async (query, root, args, ctx) => {
             return prisma.task.update({
                 ...query,
                 where: {
-                    id: parseInt(args.id),
+                    id: parseInt(args.taskId),
                 },
                 data: {
                     completed: args.completed,
@@ -79,13 +115,13 @@ builder.mutationFields((t) => ({
     deleteTask: t.prismaField({
         type: 'Task',
         args: {
-            id: t.arg.id({ required: true }),
+            taskId: t.arg.id({ required: true }),
         },
         resolve: async (query, root, args, ctx) => {
             return prisma.task.delete({
                 ...query,
                 where: {
-                    id: parseInt(args.id),
+                    id: parseInt(args.taskId),
                 }
             })
         }
